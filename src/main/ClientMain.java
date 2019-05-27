fa920e2c01a963cd3082dbd6f9d9befa473acfd5
kylesadler <kylesadler5@gmail.com>
2019-05-27 16:54:51 -0500

    implemented client connect function

--- src/main/ClientMain.java
+++ src/main/ClientMain.java
@@ -1,9 +1,24 @@
 package main;
+import java.io.*;
+import java.net.*;
 
 public class ClientMain {
-
+	
+	BufferedReader connection;
+	
+	public void connect(){
+		try{
+			Socket socket = new Socket("",); // conect to server
+			
+			connection = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+		}
+	}
+	
+	public void read
+	
 	public static void main(String[] args) {
-		
+		ClientMain client = new ClientMain();
+		client.connect();
 	}
 
 }
--- src/main/ServerMain.java
+++ src/main/ServerMain.java
@@ -1,4 +1,6 @@
 package main;
+import java.io.*;
+import java.net.*;
 
 public class ServerMain {
 
